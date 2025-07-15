f2d7be5c52372a1a565919fc6403d119e648a970
ZUXXSU <hardik.kolge.it@gmail.com>
2025-07-16 00:57:32 +0530

    Add files via upload

---
+++ I'm Rich/I'm Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm Rich
+//
+//  Created by Hardik Kolge on 13/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
